bc2eedf35ec5a70131e288baa0992752810ebf69
Jose Villa <villa1997.jv@gmail.com>
2025-01-29 14:01:30 -0600

    Add project files.

---
+++ EX2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration
+            int secret = 0;
+            int guess = 0;
+
+            bool winning = false; // flag to reflect the winning condition
+
+            bool playAgain = true;
+            string answerPlayAgain = true;
+
+            //computer generate a random secret number
+
+            Random rnd = new Random();
+            secret = rnd.Next(secret);
+
+            //proccess to play again
+            while (playAgain = true)
+            {
+
+                //program will ask user to input a number to guess secret number, until win
+                while (winning == false)
+                {
+                    Console.WriteLine("What is your guess number");
+                    guess = Convert.ToInt32(Console.ReadLine());
+
+
+                    //if the guessing number is greater than, print the number is too large
+                    if (guess > secret)
+                    {
+                        Console.WriteLine("Number is too large");
+                    }
+                    //if the guessing number is less than, print out the number is to small
+                    else if (guess < secret)
+                    {
+                        Console.WriteLine("Number is too small");
+                    }
+                    // if the guessing number is secret, you win and exit
+                    else
+                    {
+                        Console.WriteLine("you have Won!");
+                        winning = true;
+                    }
+                    Console.WriteLine("Play again? (Y/N)");
+
+                    answerPlayAgain = Console.ReadLine();
+
+
+                }
+            }
+            
+            Console.WriteLine("BYE!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EX2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EX2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EX2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4987e039-270c-43ed-98cd-0ad8781986f1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
